00806d6d263e289dc65932ae1eb53ab4b98a01c5
wagnermateus <wagnermateus7@gmail.com>
2023-04-16 23:23:58 +0100

    created Highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
+import { Highlight } from "@components/Highlight";
 
 export function Groups() {
   return (
     <Container>
       <Header showBackButton />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
     </Container>
   );
 }
