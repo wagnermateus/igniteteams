01226b132d13686948b2de5bb9011e54b05edcb7
wagnermateus <wagnermateus7@gmail.com>
2023-04-17 13:15:08 +0100

    created GroupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,15 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header showBackButton />
-      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+      <Highlight title="Turmas" subtitle="Jogue com sua turma" />
+
+      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 }
