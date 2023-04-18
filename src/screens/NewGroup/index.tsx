b0d20fb9dd68101151229c88f363d25bd5a3437e
wagnermateus <wagnermateus7@gmail.com>
2023-04-18 12:13:52 +0100

    created Input Component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -16,8 +17,8 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
-
-        <Button title="Criar" />
+        <Input placeholder="Nome da turma" />
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   );
